28a59c2d1bc54897d43b8de1644248b742c4d1fd
Noel Abellanosa <nabellan@uncc.edu>
2020-03-30 04:22:32 +0000

    Add tasks as a resource and create controller and view: - Add :tasks as a resource in routes - Generate tasks controller - Define new method in tasks controller - Define create method in tasks controller, shows raw hash of data inputted - Create tasks new.html.erb - Create tasks index.html.erb - Define initial form to create a new task, form sends post and returns to tasks/index

--- Roominder_App/config/routes.rb
+++ Roominder_App/config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get 'taskview/index'
   get 'join/index'
   get 'welcome/index'
+  
+  resources :tasks
+  
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
