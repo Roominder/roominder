3173954c803cdecae713487a884ac54ff12f5d83
Venkata <vachanta@uncc.edu>
2020-03-30 20:29:46 +0000

    Edited Welcome page

--- Roominder_App/config/environments/development.rb
+++ Roominder_App/config/environments/development.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
+  config.web_console.whitelisted_ips = ['10.0.0.0/8', '127.0.0.1/12', '136.56.54.119/16']
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
