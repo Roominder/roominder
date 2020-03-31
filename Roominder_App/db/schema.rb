8f7b509990ae4627b32720b5ff2adf2fe774187d
Yoel Kiflezghi <ykiflezg@uncc.edu>
2020-03-31 17:18:17 +0000

    Added Task relation to User

--- Roominder_App/db/schema.rb
+++ Roominder_App/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_31_132027) do
+ActiveRecord::Schema.define(version: 2020_03_31_163937) do
 
   create_table "rooms", force: :cascade do |t|
     t.string "code"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 2020_03_31_132027) do
     t.string "frequency"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
