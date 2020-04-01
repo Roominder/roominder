7860f88ccaae63bb2cad75d65ab5552cdd3007c2
yoelio <yoswed123@gmail.com>
2020-04-01 00:05:06 -0400

    Modified links to reflect new routes and display task view by room id

--- Roominder_App/app/controllers/users_controller.rb
+++ Roominder_App/app/controllers/users_controller.rb
@@ -3,5 +3,12 @@ class UsersController < ApplicationController
     end
     
     def create
+        @room = Room.find(params[:room_id])
+        @user = @room.users.create(user_params)
     end
+
+    private
+        def user_params
+            params.require(:user).permit(:name, :username)
+        end
 end
