d46576ad468dced6d401f1b7cdc28c8453c2eb70
yoelio <yoswed123@gmail.com>
2020-04-01 16:16:49 -0400

    Added password field to User model

--- Roominder_App/db/schema.rb
+++ Roominder_App/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_31_163937) do
+ActiveRecord::Schema.define(version: 2020_04_01_194237) do
 
   create_table "rooms", force: :cascade do |t|
     t.string "code"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2020_03_31_163937) do
     t.integer "room_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password"
     t.index ["room_id"], name: "index_users_on_room_id"
   end
 
