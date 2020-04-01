10febd12233fc23943e0e4136206ee73aa41bda2
yoelio <yoswed123@gmail.com>
2020-04-01 17:26:51 -0400

    Created login form with insecure authentication

--- Roominder_App/app/controllers/rooms_controller.rb
+++ Roominder_App/app/controllers/rooms_controller.rb
@@ -1,4 +1,16 @@
 class RoomsController < ApplicationController
+    def index
+        # TODO: Hash passwords, instead of storing them in plain text
+        @user = User.find_by(username: params[:username], password: params[:password])
+        if @user != nil
+            redirect_to "/rooms/%s/users/%s/tasks" % [@user.room_id, @user.id]
+        else
+            # Indicates that the user was not found with given credentials
+            # We can redirect to a try again page here if we like
+            redirect_to "/"
+        end
+    end
+
     def new
     end
     
